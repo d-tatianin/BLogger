3fd653a8be662cd50b42ce360078135000403142
8infy <34326714+8infy@users.noreply.github.com>
2019-09-19 20:44:04 +0300

    Fixed a macro name.

--- include/BLogger/Formatter/FormatUtilities.h
+++ include/BLogger/Formatter/FormatUtilities.h
@@ -7,7 +7,7 @@
 // but is still not fixed.
 // So we use this workaround:
 #if _MSVC_LANG >= 201703L || __cplusplus >= 201703L
-    #define BLOGGER_PACK_BEGIN(formatter, args) (formatter.handle_pack(std::forward<Args>(args)), ...)
+    #define BLOGGER_PROCESS_PACK(formatter, args) (formatter.handle_pack(std::forward<Args>(args)), ...)
 #elif _MSVC_LANG >= 201402L || __cplusplus >= 201402L
     #define BLOGGER_PROCESS_PACK(formatter, args) int expander[] = { 0, ( (void) formatter.handle_pack(std::forward<Args>(args)), 0) ... }
 #else
