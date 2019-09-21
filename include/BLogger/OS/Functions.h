d912acad7435bf4389e9887582bd3ea4d76d192e
8infy <99danilt@gmail.com>
2019-09-21 12:50:05 +0300

    BLogger now compiles under UNIX.

--- include/BLogger/OS/Functions.h
+++ include/BLogger/OS/Functions.h
@@ -10,6 +10,8 @@
     #define MEMORY_MOVE(dst, dst_size, src, src_size) memmove_s(dst, dst_size, src, src_size)
     #define STACK_ALLOC(size) _malloca(size)
 #else
+    #include <cstring>
+    #include <algorithm>
     #define UPDATE_TIME(to, from) localtime_r(&from, &to)
     #define OPEN_FILE(file, path) file = fopen(path.c_str(), "w")
     #define MEMORY_COPY(dst, dst_size, src, src_size) memcpy(dst, src, src_size)
